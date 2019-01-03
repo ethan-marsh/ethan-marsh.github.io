4ebfa3edd8b6a1f3b8225710514a0dfeffb1e053
Ethans-Mac-mini <ethancmarsh@yahoo.com>
2019-01-03 00:37:06 -0800

    portfoliio grid layout

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,18 +4,25 @@ export default class Portfolio extends Component {
   render() {
     return (
       <Fragment >
-          <ul className="porfolio__categories">
+          <ul className="portfolio__categories">
           {['All', 'Professional','Personal','Playground'].map(link => (
-              <li key={link}><a className="portoflio__link" href="#">{link}</a></li>
+              <li key={link}><a className="portfolio__link" href={`#${link}`}>{link}</a></li>
               ))}
             </ul>
-
           <div className="portfolio__projects projects">
-          <a href="#" className="projects-link">
-              <figure className="projects-figure"></figure> </a>
-              <a href="#" className="projects-link"> <figure className="projects-figure"></figure> </a>
-              <a href="#" className="projects-link">
-              <figure className="projects-figure"></figure> </a>
+          <a href="#project" className="projects-link">
+              <figure className="projects-figure"><img src="images/hero.jpg" alt="project" /></figure></a>
+              <a href="#project" className="projects-link"> <figure className="projects-figure">
+              <img src="images/hero.jpg" alt="project" />
+              </figure></a>
+              <a href="#project" className="projects-link">
+              <figure className="projects-figure">
+              <img src="images/hero.jpg" alt="project" />
+              </figure></a>
+              <a href="#project" className="projects-link">
+              <figure className="projects-figure">
+              <img src="images/hero.jpg" alt="project" />
+              </figure></a>
 
           </div>
       </Fragment>
