7a3df687988548ac9d7e2e00f318b3e250ae5bdd
Ethans-Macbook-Pro <ecmarsh@ucdavis.edu>
2019-01-03 13:15:40 -0800

    Update temp images

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,41 +1,41 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component, Fragment } from "react";
 
 const jobs = {
   job1: {
-    title: 'Penske Motor Group',
+    title: "Penske Motor Group",
     years: 3,
-    yearSpan: '2017-19',
-    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat consectetur adipisci veritatis nobis, autem quibusdam dicta aspernatur magni porro, Neque voluptates aliquid impedit.',
+    yearSpan: "2017-19",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat consectetur adipisci veritatis nobis, autem quibusdam dicta aspernatur magni porro, Neque voluptates aliquid impedit."
   },
   job2: {
-    title: 'Penske Motor Group',
+    title: "Penske Motor Group",
     years: 1,
-    yearSpan: '2015-16',
-    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat consectetur adipisci veritatis nobis, autem quibusdam dicta aspernatur magni porro, Neque voluptates aliquid impedit.',
+    yearSpan: "2015-16",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat consectetur adipisci veritatis nobis, autem quibusdam dicta aspernatur magni porro, Neque voluptates aliquid impedit."
   },
   job3: {
-    title: 'MBUSA',
+    title: "MBUSA",
     years: 1,
-    yearSpan: '2014-15',
-    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat consectetur adipisci veritatis nobis, autem quibusdam dicta aspernatur magni porro, Neque voluptates aliquid impedit.',
-  },
+    yearSpan: "2014-15",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat consectetur adipisci veritatis nobis, autem quibusdam dicta aspernatur magni porro, Neque voluptates aliquid impedit."
+  }
 };
 
 const certifications = {
   cert1: {
-    title: 'Harvard CS50',
-    time: '2018',
+    title: "Harvard CS50",
+    time: "2018"
   },
   cert2: {
-    title: 'B.S, University of California, Davis',
-    time: '2014',
-  },
-}
-
+    title: "B.S, University of California, Davis",
+    time: "2014"
+  }
+};
 
 export default class Experience extends Component {
-
-
   render() {
     return (
       <Fragment>
@@ -43,24 +43,37 @@ export default class Experience extends Component {
           <h4 className="experience__title-h4">Experience.</h4>
         </div>
         <div className="experience-right">
-        <ul className="experience__list">
-        {Object.keys(jobs).map(key => (
-          <li className="experience__item" key={key}>
-              <h5 className="time__title">{jobs[key].years} Years</h5>
-              <p className="time__sub">{jobs[key].yearSpan}</p>
-              <h5 className="description__title">{jobs[key].title}</h5>
-              <p className="description__sub">{jobs[key].description}</p>
-          </li>
-        ))}
-        {Object.keys(certifications).map(key => (
-          <li className="experience__item experience__item--certs" key={key}>
-              <h5 className="time__title">{certifications[key].time}</h5>
-              <h5 className="description__title">{certifications[key].title}</h5>
-          </li>
-        ))}
-        </ul>
+          <ul className="experience__list">
+            {Object.keys(jobs).map(key => (
+              <li className="experience__item" key={key}>
+                <div className="experience-time time">
+                  <h5 className="time__title">{jobs[key].years} Years</h5>
+                  <p className="time__sub">{jobs[key].yearSpan}</p>
+                </div>
+                <div className="experience-description description">
+                  <h5 className="description__title">{jobs[key].title}</h5>
+                  <p className="description__sub">{jobs[key].description}</p>
+                </div>
+              </li>
+            ))}
+            {Object.keys(certifications).map(key => (
+              <li
+                className="experience__item experience__item--certs"
+                key={key}
+              >
+                <div className="experience-time time">
+                  <h5 className="time__title">{certifications[key].years}</h5>
+                </div>
+                <div className="experience-description description">
+                  <h5 className="description__title">
+                    {certifications[key].title}
+                  </h5>
+                </div>
+              </li>
+            ))}
+          </ul>
         </div>
       </Fragment>
-    )
+    );
   }
 }
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,31 +1,41 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component, Fragment } from "react";
 
 export default class Portfolio extends Component {
   render() {
     return (
-      <Fragment >
-          <ul className="portfolio__categories">
-          {['All', 'Professional','Personal','Playground'].map(link => (
-              <li key={link}><a className="portfolio__link" href={`#${link}`}>{link}</a></li>
-              ))}
-            </ul>
-          <div className="portfolio__projects projects">
-          <a href="#project" className="projects-link">
-              <figure className="projects-figure"><img src="images/hero.jpg" alt="project" /></figure></a>
-              <a href="#project" className="projects-link"> <figure className="projects-figure">
-              <img src="images/hero.jpg" alt="project" />
-              </figure></a>
-              <a href="#project" className="projects-link">
+      <Fragment>
+        <ul className="portfolio__categories">
+          {["All", "Professional", "Personal", "Playground"].map(link => (
+            <li key={link}>
+              <a className="portfolio__link" href={`#${link}`}>
+                {link}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="portfolio__projects projects">
+          {[1, 2, 3, 4, 5, 6].map(projectNum => (
+            <a
+              key={projectNum}
+              href={`#project-${projectNum}`}
+              className="projects-link"
+            >
               <figure className="projects-figure">
-              <img src="images/hero.jpg" alt="project" />
-              </figure></a>
-              <a href="#project" className="projects-link">
-              <figure className="projects-figure">
-              <img src="images/hero.jpg" alt="project" />
-              </figure></a>
-
-          </div>
+                <img
+                  src={`images/project-${projectNum}.jpg`}
+                  alt={`Project ${projectNum}`}
+                />
+                <figcaption>
+                  <h5>{`Project ${projectNum}`}</h5>
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  </p>
+                </figcaption>
+              </figure>
+            </a>
+          ))}
+        </div>
       </Fragment>
-    )
+    );
   }
 }
