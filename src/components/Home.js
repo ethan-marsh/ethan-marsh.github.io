54806c1609d6bf429480e3b5b729b282a7637418
Ethans-Mac-mini <ethancmarsh@yahoo.com>
2019-01-05 21:34:35 -0800

    Home changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -44,60 +44,62 @@ export default class Home extends Component {
         onClick={this.cdm}
         height={this.homeHeight || "100%"}
       >
+
        <div className="home__title">
-       <Transition
-        native
-        items={{item:true}}
-        from={{ opacity: 0}}
-        enter={{opacity: 1}}
-        leave={{opacity: 0}}
-        config={{...config.molasses }}>
-        {() =>
-          props =>
-            <animated.h1
-              className="home__title-h1" style={props} children={'Ethan Marsh'}
-            />
-        }
-        </Transition>
+          <Transition
+            native
+            items={{ item: true }}
+            from={{ opacity: 0 }}
+            enter={{ opacity: 1 }}
+            leave={{ opacity: 0 }}
+            config={{ ...config.molasses }}>
+            {() =>
+              props =>
+                <animated.h1
+                  className="home__title-h1" style={props} children={'Ethan Marsh'}
+                />
+            }
+          </Transition>
         </div>
+
         <Transition
-        native
-        items={divider}
-        from={{ transform: 'translate3d(-100%,0,0)', opacity: 0}}
-        enter={{ transform: 'translate3d(0,0,0)', opacity: 1}}
-        leave={{ transform: 'translate3d(100%,0,0)', opacity: 0}}
-        config={{...config.molasses, duration: 800}}
+          native
+          items={divider}
+          from={{ transform: 'translate3d(-100%,0,0)', opacity: 0 }}
+          enter={{ transform: 'translate3d(0,0,0)', opacity: 1 }}
+          leave={{ transform: 'translate3d(100%,0,0)', opacity: 0 }}
+          config={{ ...config.molasses, duration: 800 }}
         >
-        {divider => (props =>
-            <animated.div style={props}className="home__divider" children={divider}/>
+          {divider => (props =>
+            <animated.div style={props} className="home__divider" children={divider} />
           )}
         </Transition>
 
         <Transition
-        native
-        from={{ transform: 'translate3d(0,-2rem,0)', opacity: 0}}
-        enter={{transform: 'translate3d(0,0,0)', opacity: 1}}
-        leave={{opacity: 0}}
-        config={{...config.molasses}}
-        delay={800}>
-
-        {() => (props =>
+          native
+          from={{ transform: 'translate3d(0,-2rem,0)', opacity: 0 }}
+          enter={{ transform: 'translate3d(0,0,0)', opacity: 1 }}
+          leave={{ opacity: 0 }}
+          config={{ ...config.molasses }}
+          delay={800}>
+          {() => (props =>
             <animated.div className="home__bottom" style={props}>
               <h2 className="home__bottom-subtitle" >
                 Front-End Web Developer / San Francisco
               </h2>
-
               <ButtonMore
                 className="home__bottom-more"
                 onClick={e => this.scrollToAbout(e.currentTarget)}
               >
                 &or;
               </ButtonMore>
-
-              <div className="home__bottom__links" ><Social/></div>
+              <div className="home__bottom__links" >
+                <Social />
+              </div>
             </animated.div>
-        )}
-           </Transition>
+          )}
+        </Transition>
+
       </Content>
     );
   }
