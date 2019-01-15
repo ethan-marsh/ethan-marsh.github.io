67f41fa7a931619631d02d1ebfe0743ddbf540f3
Ethans-Macbook-Pro <ecmarsh@ucdavis.edu>
2019-01-15 15:46:40 -0800

    Update theme

--- src/components/Page.js
+++ src/components/Page.js
@@ -4,24 +4,31 @@ import Meta from "./Meta";
 import Header from "./Header";
 
 const theme = {
-  red: "#FF0000",
-  black: "#393939",
-  white: "#eee",
+  blue: "#005DB3",
+  black: "#000000",
+  white: "#FFFFFF",
   grey: "#3A3A3A",
+  darkblue: "#02243C",
+  lightblue: "#5195CE",
   lightgrey: "#E1E1E1",
   offWhite: "#EDEDED",
-  maxWidth: "1000px",
+  maxWidth: "1440px",
+  fontPrimary: "'Helvetica Neue', 'Open Sans', sans-serif;",
+  fontDisplay: "'Raleway', sans-serif",
   bs: "0 12px 24px 0 rgba(0, 0, 0, 0.09)"
 };
 
 const StyledPage = styled.div`
-  background: white;
-  color: ${props => props.theme.white};
+  background: ${props => props.theme.white};
+  color: ${props => props.theme.black};
 `;
 
 const Inner = styled.div`
   margin: 0 auto;
   padding: 0;
+  font-family: ${props => props.theme.fontPrimary};
+  font-weight: 300;
+  -webkit-font-smoothing: antialiased;
 `;
 
 createGlobalStyle`
@@ -31,12 +38,14 @@ createGlobalStyle`
   }
   *, *:before, *:after {
     box-sizing: inherit;
+    margin: 0;
+    padding: 0;
   }
   body {
     padding: 0;
     margin: 0;
   }
-  a {
+  a:link, a:visited {
     text-decoration: none;
     color: ${theme.black};
   }
