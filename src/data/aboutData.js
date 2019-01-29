648d00f158fd2840fcad570885d520e083a4f121
Ethans-Mac-mini <ethancmarsh@yahoo.com>
2019-01-29 01:56:02 -0800

    Update skills

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,26 +1,36 @@
 const aboutText = {
-    p1: `
+  p1: `
     Hey I'm Ethan, front-end developer in San Francisco. I've been programming for the past 5 years, the last 3 years focused on modern web development and design, overseeing the launch of two websites and numerous apps. I love learning how things work, looking at things in new ways, and making meaningful improvements to processes & products. Needless to say, I love the fast-pace and variety web dev offers.
     `,
-    p2: `
+  p2: `
     My current position relies heavily on using my business knowledge to improve things via technology. From a technical standpoint, I spend most of my time working with Javascript, CSS3, HTML5 (and VBA). My personal projects have allowed me to try & participate in the new web technologies, and am looking to utilize and continue improving these skills.
     `,
-    p3: `
+  p3: `
     Take a deep dive into my skillset, explore my work below, get in touch, or just enjoy the site!
     `,
-}
+};
 
 const aboutSkills = {
-    base: [
-        'git, bash, zsh'
-    ],
-    core: [
-        'Javscript, CSS3 (Sass/Stylus)', 'HTML5', 'Mobile & Responsive Design', 'Node.js (basics)', 'MySQL'
-    ],
-    libraries: [
-        'React.js', 'Gulp', 'Wordpress', 'jQuery', 'Bootstrap'
-    ]
-}
+  core: [
+    'Javscript',
+    'CSS3 ',
+    'HTML5',
+    'Mobile & Responsive Design',
+    'Node.js (basics)',
+
+    'git',
+    'CLI: bash/zsh',
+  ],
+  libraries: [
+    'React.js',
+    'Redux',
+    'Testing: Jest/Enzyme',
+    'Gulp',
+    'jQuery',
+    'Sass || Stylus',
+    'Bootstrap',
+  ],
+  other: ['Excel VBA ', 'C (basic)', 'MySQL', 'Sketch', 'Adobe PS + AI'],
+};
 
 export { aboutText, aboutSkills };
-
