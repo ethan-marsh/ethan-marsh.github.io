15db203ade8aa411a8dd2598c652a640bc471af8
Ethans-Mac-mini <ethancmarsh@yahoo.com>
2019-01-29 01:57:14 -0800

    Text change

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 const aboutText = {
   p1: `
-    Hey I'm Ethan, front-end developer in San Francisco. I've been programming for the past 5 years, the last 3 years focused on modern web development and design, overseeing the launch of two websites and numerous apps. I love learning how things work, looking at things in new ways, and making meaningful improvements to processes & products. Needless to say, I love the fast-pace and variety web dev offers.
+    Hey I'm Ethan, front-end developer in San Francisco. I've been programming for the past 5 years, the last 3 years focused on modern web development and design, overseeing the launch of two websites and numerous apps. I love learning how things work, looking at things in new ways, and making meaningful improvements to processes & products.
     `,
   p2: `
     My current position relies heavily on using my business knowledge to improve things via technology. From a technical standpoint, I spend most of my time working with Javascript, CSS3, HTML5 (and VBA). My personal projects have allowed me to try & participate in the new web technologies, and am looking to utilize and continue improving these skills.
